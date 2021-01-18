62b2d4767f875e3ae7cfbbbc4e2927a3d36414fa
Liz Burke <liz.burke.chi@gmail.com>
2021-01-18 19:15:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  data.each_with_object({}) do |(key, value), finished_array|
+    #binding.pry
+    value.each do |inner_key, pigeon_names|
+      pigeon_names.each do |name|
+        binding.pry
+        if !finished_array[name]
+          finished_array[name] = {}
+        end
+
+      end
+      finished_array
+    end
+  binding.pry
 end
